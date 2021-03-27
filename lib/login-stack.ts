8b6d8d4d05ac999a082720556bfa697ddb987f56
Alexandre Ferrera <alexandre78ferrera@gmail.com>
2021-03-27 01:09:44 +0100

    dynamodb users table created
    DynamoDB usersTable created in the stack

--- lib/login-stack.ts
+++ lib/login-stack.ts
@@ -2,21 +2,33 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
 import { LambdaIntegration } from '@aws-cdk/aws-apigateway';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
 
 export class LoginStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const usersTable = new dynamodb.Table(this, 'Users', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
     const getRegisterHandler = new lambda.Function(this, 'registerHandler', {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambda/register'),
-      handler: 'register.handler'
+      handler: 'register.handler',
+      environment: {
+        usersTableName: usersTable.tableName,
+      }
     });
 
     const getLoginHandler = new lambda.Function(this, 'loginHandler', {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambda/login'),
-      handler: 'login.handler'
+      handler: 'login.handler',
+      environment: {
+        usersTableName: usersTable.tableName,
+      }
     });
 
     const api = new apigw.RestApi(this, 'ubiquitab-api', { });
